6e16b4fdbf32651a2d573139bfc669fcb85c189c
YeJin Lee <bambi_21@daum.net>
2021-02-09 23:59:53 +0900

    PS: Add 2869 in BOJ

---
+++ PS/BOJ/S8_2869.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class S8_2869 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int A = Integer.valueOf(st.nextToken());
+		int B = Integer.valueOf(st.nextToken());
+		int V = Integer.valueOf(st.nextToken());
+		
+		int pos = 0;
+		int day = (V-B) / (A-B);
+		
+		if((V-B)%(A-B)!=0) {
+			day++;
+			
+		}
+		System.out.println(day);
+	}
+
+}
