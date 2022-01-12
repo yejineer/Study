c5dfd813ca894a6089fb153c4c7ada69c9462e85
YeJin Lee <bambi_21@daum.net>
2022-01-12 23:58:34 +0900

    BOJ: Add java file(15654)

---
+++ PS/BOJ/Code/Q15654.java
@@ -0,0 +1,54 @@
+package Backtracking;
+/*
+    [문제] N과 M (5)
+    [분류] 백트래킹
+    [날짜] 2022-01-12
+ */
+import java.util.*;
+import java.io.*;
+
+public class Q15654 {
+    static int N, M;
+    static int[] arr;
+    static int[] selected;
+    static boolean[] visited;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        arr = new int[N];
+        selected = new int[N];
+        visited = new boolean[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+
+        dfs(0);
+        System.out.print(sb.toString());
+    }
+
+    public static void dfs(int depth) {
+        if (depth == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(selected[i] + " ");
+            }
+            sb.append("\N");
+            return;
+        }
+        for (int i = 0; i < N; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                selected[depth] = arr[i];
+                dfs(depth + 1);
+                visited[i] = false;
+            }
+        }
+    }
+}
